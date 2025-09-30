3c4f83ae57d9e57e5bbf9c365670dbccf05fc336
BuiHuuTrung123 <trungbuimanifest@gmail.com>
2025-09-30 11:40:17 +0700

    add skills

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Sidebar from './components/Sidebar';
 import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 function App() {
   return (
     <Box sx={{ display: 'flex', bgcolor: 'background.default' }}>
       <Sidebar />
       <Container component="main" sx={{ flexGrow: 1, p: { xs: 2, lg: 5 }, py: 4 }}>
         <About />
-          <Experience />
-             <Projects/>
+        <Experience />
+        <Projects />
+        <Skills />
       </Container>
-   
+
     </Box>
   );
 }
