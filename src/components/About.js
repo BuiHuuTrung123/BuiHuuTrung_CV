256004e4b75a0b885ef5d5743587b22a3e597acd
BuiHuuTrung123 <trungbuimanifest@gmail.com>
2025-09-30 11:17:44 +0700

    add experience

--- src/components/About.js
+++ src/components/About.js
@@ -10,18 +10,18 @@ function About() {
         BUI HUU <span style={{ color: 'primary.main' }}>TRUNG</span>
       </Typography>
       <Typography variant="h6" sx={{ mb: 1, display: 'flex', alignItems: 'center', gap: 1 }}>
-        <i className="fa fa-address-card" /> Nhà phát triển Web nhiệt huyết
+        <i className="fa fa-address-card" />  Nhà phát triển web nhiệt huyết 
 
       </Typography>
       <Typography variant="h6" sx={{ mb: 4, display: 'flex', alignItems: 'center', gap: 1 }}>
-        <i className="fa fa-map" /> Quận Hà Đông, Hà Nội, Việt Nam
+        <i className="fa fa-map" />Quận Hà Đông, Hà Nội, Việt Nam
       </Typography>
       <Paper elevation={3} sx={{ p: 3, mb: 4, bgcolor: 'primary.light', borderRadius: 2 }}>
         <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
           Xin chào, chào mừng bạn đến với hồ sơ của tôi!
         </Typography>
         <Typography variant="body1">
-          Tôi là Bùi Hữu Trung, sinh viên công nghệ thông tin năm cuối của trường đại học Công Nghệ Giao Thông Vận Tải với niềm đam mê lập trình Web. Tôi luôn tìm kiếm cơ hội để học hỏi, trau dồi kỹ năng và áp dụng công nghệ vào giải quyết các bài toán thực tế
+          Tôi là Bùi Hữu Trung, sinh viên công nghệ thông tin năm cuối của trường đại học Công Nghệ Giao Thông Vận Tải với niềm đam mê lập trình Web. Tôi luôn tìm kiếm cơ hội để học hỏi, trau dồi kỹ năng và áp dụng công nghệ vào giải quyết các bài toán thực tế.
         </Typography>
         <Typography variant="body1">
           - Mục tiêu ngắn hạn: Tìm kiếm cơ hội thực tập/làm việc để phát triển kỹ năng Front-end & Back-end.
@@ -29,11 +29,11 @@ function About() {
         </Typography>
         <Typography>- Mục tiêu dài hạn: Trở thành một lập trình viên Full-stack, đóng góp vào những sản phẩm chất lượng và hữu ích cho cộng đồng.</Typography>
       </Paper>
-      <Typography variant="body1" sx={{ mb: 2 }}>
+      <Typography variant="h6" sx={{ mb: 2 }}>
         <strong><i className="fab fa-react" style={{ color: '#47CEF6' }} /> Front-end:</strong> ReactJS (Hook,
         Redux), Material UI, Axios.
       </Typography>
-      <Typography variant="body1" sx={{ mb: 4 }}>
+      <Typography variant="h6" sx={{ mb: 4 }}>
         <strong><i className="fab fa-node-js" style={{ color: '#88C048' }} /> Back-end:</strong> Node.JS,
         ExpressJS, MongoDB, MySQL.
       </Typography>
